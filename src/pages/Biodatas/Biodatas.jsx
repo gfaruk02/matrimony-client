a7abb789532a0a77f87fb1e371acba795218e4fc
gfaruk02 <gfaruk02@gmail.com>
2023-11-29 12:49:01 +0600

    filter function in biodatas page and set filter design

--- src/pages/Biodatas/Biodatas.jsx
+++ src/pages/Biodatas/Biodatas.jsx
@@ -5,44 +5,31 @@ import ShowBiodatas from "./ShowBiodatas";
 
 const Biodatas = () => {
     const biodatas = useBiodata()
-    // console.log(biodatas);
+    const [minAge, setMinAge] = useState('')
+    const [maxAge, setMaxAge] = useState('')
+    const [gender, setGender] = useState('')
+    const [division, setDivision] = useState('')
     const [filteredBiodata, setFilteredBiodata] = useState([]);
-    const [filters, setFilters] = useState({
-        minAge: '',
-        maxAge: '',
-        gender: 'all',
-        division:'all',
-    });
-    console.log(filters);
+    console.log(filteredBiodata);
     useEffect(() => {
         setFilteredBiodata(biodatas);
-      }, [biodatas]);
+    }, [biodatas]);
 
+    // console.log(biodatas);
+    const handleSearch = () => {
+        const filterAges = biodatas?.filter((person) => {
+            const min = parseInt(minAge)
+            const max = parseInt(maxAge)
+            return (
+                (person.age >= min && person.age <= max) &&
+                (gender == person.biodataType) &&
+                (division == person.permanentDivision)
 
-    // const [biodata, setBiodata] = useState(biodatas)
-    // console.log(biodata);
-    const handleSearch = (e) =>{
-    //    console.log(e);
-        const newfilteredBiodata = biodatas?.filter((data)=>{
-            // console.log(data);
-            const age = parseInt(data.age);
-            // console.log(age);
-            const minAge =(filters.minAge)
-            console.log(minAge);
-            const maxAge =(filters.maxAge);
-            console.log(minAge);
-            return(
-                (age>= minAge && age <= maxAge) &&
-                (filters.gender == data.biodataType) &&
-                (filters.division == data.permanentDivision)
-    
             );
-    
+
         })
-        e.preventDefault()
-       
-        setFilteredBiodata(newfilteredBiodata)
-    };
+        setFilteredBiodata(filterAges)
+    }
 
     // console.log(biodata);
     return (
@@ -52,40 +39,39 @@ const Biodatas = () => {
                     <div className="w-full rounded-md bg-gray-900 px-5 lg:mt-20">
                         <span className="block mb-2 text-violet-400"></span>
                         <h1 className="text-5xl font-extrabold ">Filter</h1>
-                        <p className="my-8">
-                            You can filter by age [Range], biodata type
-                            [male|female] , filter by division [Dhaka,Chattagram,Rangpur,Barisal ,
-                            Khulna,Maymansign,Sylhet]
+                        <p className="my-5">
+                            Filter by age [Range] <br /> Biodata type
+                            [male|female] <br /> Division
                         </p>
-                        <div className="pt-5">
-                           <div> 
-                           <p className="text-sm">Min Age</p>
-                            <input name="minAge" type="number" placeholder="Min Age" className=" py-2 w-full rounded-md focus:ring focus:ri border-gray-700 text-gray-900 "
-                            value={filters.minAge}
-                            onChange={(e) => setFilters(e.target.value)}
-                            />
-                           </div>
-                           <div> 
-                           <p className="text-sm">Max Age</p>
-                            <input name="maxAge" type="number"  placeholder="Your name" className=" py-2 w-full rounded-md focus:ring focus:ri border-gray-700 text-gray-900" 
-                            value={filters.maxAge}
-                            onChange={(e) => setFilters(e.target.value)}
-                            />
-                           </div> 
+                        <div className="pt-2">
+                            <div>
+                                <p className=" py-1 text-sm">Min Age</p>
+                                <input name="minAge" type="number" placeholder="Min Age" className=" py-2 pl-2 w-full rounded-md focus:ring focus:ri border-gray-700 text-gray-900 "
+                                    value={minAge}
+                                    onChange={(e) => setMinAge(e.target.value)}
+                                />
+                            </div>
+                            <div>
+                                <p className=" py-1 text-sm">Max Age</p>
+                                <input name="maxAge" type="number" placeholder="Your name" className=" py-2 pl-2 w-full rounded-md focus:ring focus:ri border-gray-700 text-gray-900"
+                                    value={maxAge}
+                                    onChange={(e) => setMaxAge(e.target.value)}
+                                />
+                            </div>
                         </div>
-                        <div className="pt-5">
-                            <p className="text-sm">Biodata Type [male|female]</p>
-                            <select className="text-gray-900 py-2 px-10" name="gender" value={filters.gender}
-                            onChange={(e) => setFilters(e.target.value)}> 
+                        <div className="pt-2">
+                            <p className=" py-1 text-sm">Biodata Type [male|female]</p>
+                            <select className="text-gray-900 py-2 rounded-lg px-10" name="gender" value={gender}
+                                onChange={(e) => setGender(e.target.value)}>
                                 <option value="all"> all</option>
                                 <option value="Male"> Male</option>
                                 <option value="Female"> Female</option>
                             </select >
                         </div>
                         <div className="col-span-full text-gray-900 sm:col-span-2 pt-2">
-                            <p className="text-sm">Division</p>
-                            <select className="select py-1 rounded-lg px-24 select-bordered" type="text" name="division" value={filters.division}
-                            onChange={(e) => setFilters(e.target.value)}> 
+                            <p className=" py-1 text-sm text-white">Division</p>
+                            <select className="select py-2 rounded-lg px-24 select-bordered" type="text" name="division" value={division}
+                                onChange={(e) => setDivision(e.target.value)}>
                                 <option value="all">all</option>
                                 <option value="Dhaka"> Dhaka</option>
                                 <option value="Chattagram"> Chattagram</option>
@@ -96,19 +82,8 @@ const Biodatas = () => {
                                 <option value="Sylhet">Sylhet</option>
                             </select>
                         </div>
-                        {/* <div className="col-span-full text-gray-900 sm:col-span-2">
-                <label className="text-sm text-white">Race</label>
-  <select name="race" className="select py-1 rounded-lg px-24 select-bordered">
-    <option disabled selected>Pick one</option>
-    <option>Bangali</option>
-    <option>Chakma</option>
 
-  </select>
-  
-				</div> */}
-
-
-                        <button onClick={handleSearch} type="button" className="w-full mt-5 py-3 font-semibold rounded bg-violet-400 text-gray-900">Join the waitlist</button>
+                        <button onClick={handleSearch} type="button" className="w-full mt-5 py-3 font-semibold rounded bg-rose-400 text-gray-900">Filter Biodata</button>
                     </div>
 
                     <div className="w-full mx-auto col-span-2 text-center">
